2a1cf45139ac9ba3a9b5165e06414c688f789e55
Anton Sviridov <keynmol@gmail.com>
2021-12-05 11:19:58 +0000

    Add validation of samples

---
+++ validate.scala
@@ -0,0 +1,40 @@
+// using scala 3.0.2
+// using lib com.lihaoyi::os-lib:0.7.8
+// using lib com.eed3si9n.expecty::expecty:0.15.4
+
+import scala.util.Try
+import com.eed3si9n.expecty.Expecty.{assert as expect}
+
+case class A(part1: Int, part2: Int):
+  def render = s"Part 1: $part1\nPart 2: $part2"
+
+val answers = Map(
+  2021 -> Map(
+    1 -> A(7, 5),
+    2 -> A(150, 900),
+    3 -> A(198, 230),
+    4 -> A(4512, 1924),
+    5 -> A(5, 12)
+  )
+)
+
+@main def hello(output: String, year: Int, day: Int) =
+  val path =
+    Try(os.Path(output)).getOrElse(os.RelPath(output).resolveFrom(os.pwd))
+
+  val contents = os.read(path)
+
+  val lines = contents.linesIterator.toList.map(_.trim).filterNot(_.isEmpty)
+
+  val part1 = lines.collectFirst { case s"Part 1: $num" =>
+    num.toInt
+  }.get
+
+  val part2 = lines.collectFirst { case s"Part 2: $num" =>
+    num.toInt
+  }.get
+
+  val answer = A(part1, part2)
+
+  expect(answers(year)(day) == answer)
+end hello
